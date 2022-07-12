507ce20aa235966f2557e70419f6a298a66bb58c
Silviu <nistorescu.silviu@gmail.com>
2022-07-12 13:34:37 +0300

    teamMembers modified

--- components/TeamHero.js
+++ components/TeamHero.js
@@ -1,7 +1,19 @@
 import TeamMember from "./TeamMember";
+import { TeamList } from "./TeamList";
 
-const TeamHero = () => {
-  return <TeamMember />;
+const TeamHero = (option) => {
+  return (
+    <div className="m-4 flex flex-shrink-0 flex-row flex-nowrap overflow-hidden bg-red-100">
+      {TeamList.map((option) => (
+        <TeamMember
+          title={option.title}
+          key={option.id}
+          description={option.text}
+          photo={option.photo}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default TeamHero;
---
+++ components/TeamList.js
@@ -0,0 +1,30 @@
+export const TeamList = [
+  {
+    title: "Dr. Matteo Iacobellis",
+    photo: "/doctor1.jpg",
+    description: "Stomatologie Generala",
+    subtitle: "Chirurgie, Stomatologie Generala",
+    id: 1,
+  },
+  {
+    title: "Dr. Lavinia Ardalau",
+    photo: "/doctor2.jpg",
+    description: "Stomatologie Generala",
+    subtitle: "Dentist Generalist si Rezident la Protetica Dentara",
+    id: 2,
+  },
+  {
+    title: "Dr. Adriana Popescu",
+    photo: "/doctor3.jpg",
+    description: "Stomatologie Generala",
+    subtitel: "Asistenta",
+    id: 3,
+  },
+  {
+    title: "Dr. Francesca Iacobellis",
+    photo: "/doctor4.jpg",
+    description: "Stomatologie Generala",
+    subtitle: "Dentist Generalist",
+    id: 4,
+  },
+];

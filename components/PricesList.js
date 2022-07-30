ba482d1ac6ab6ff56f994be6eb8408a0dc79d3d9
Silviu <nistorescu.silviu@gmail.com>
2022-07-30 18:46:26 +0300

    starting PricesList

---
+++ components/PricesList.js
@@ -0,0 +1,21 @@
+export const EstFac = [
+  {
+    title: "Estetica Faciala",
+    serviceName: [
+      "Consultatie specialist",
+      "Volumizare si conturare buze cu acid hialuronic",
+      "",
+    ],
+    servicePrice: ["Gratuit", "200€"],
+  },
+];
+
+export const Profilaxie = [
+  {
+    title: "Dr. Matteo Iacobellis",
+    photo: "/doctor1.jpg",
+    description: "Stomatologie Generala",
+    subtitle: "Chirurgie, Stomatologie Generala",
+    id: 1,
+  },
+];

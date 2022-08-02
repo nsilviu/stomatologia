6c1536b042ba42cca868e271d9e9200b279bd02f
Silviu <nistorescu.silviu@gmail.com>
2022-08-02 20:09:28 +0300

    80% prices

--- components/PriceTable.js
+++ components/PriceTable.js
@@ -1,16 +1,32 @@
 import React from "react";
-import { EstFac } from "./PricesList";
 
 function PriceTable(props) {
   const { title, serviceName, servicePrice } = props;
   return (
-    <div className="m-4 flex flex-col rounded-md bg-slate-100 text-center  text-slate-900 shadow-md">
-      <span className="mt-2 text-2xl font-bold">{title} Estetica faciala</span>
-      <div className="m-4 justify-around space-x-8">
-        {serviceName.map((option) => {
-          <span>{option}</span>;
-        })}
-        <span className="text-teal-500">{servicePrice}Gratuit</span>
+    <div className="m-4 flex flex-col rounded-md bg-white text-center  text-slate-900 shadow-md">
+      <span className="mt-2 text-2xl font-bold">{title}</span>
+      <div className=" m-4 flex flex-row justify-around">
+        <div className="flex flex-col justify-evenly">
+          {serviceName.map((option, index) => {
+            return (
+              <span
+                className="justify-evenly p-2 text-right text-xs"
+                key={index}
+              >
+                {option}
+              </span>
+            );
+          })}
+        </div>
+        <div className="flex w-1/2 flex-col justify-evenly">
+          {servicePrice.map((option, index) => {
+            return (
+              <span key={index} className="p-2 text-left text-xs text-teal-500">
+                {option}
+              </span>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
